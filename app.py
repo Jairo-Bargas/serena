14632751c2fa9860a7ee5d29ff7db41f71cbe436
Jairo-Bargas <jfacundobargas2@gmail.com>
2026-04-07 16:36:12 -0300

    Solucionando sitemap

--- app.py
+++ app.py
@@ -10,3 +10,7 @@ def home():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
+@app.route('/sitemap.xml')
+def sitemap():
+    return send_from_directory('static', 'sitemap.xml')
